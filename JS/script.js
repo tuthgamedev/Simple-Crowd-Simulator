fc6b46d92a182dff70116db6ee30c895a3c679ca
TuthDev <tuth.gamedev2024@gmail.com>
2026-07-02 14:20:51 +0700

    adding link for all version

--- JS/script.js
+++ JS/script.js
@@ -23,8 +23,8 @@
     v2: {
       title: 'VERSION 2 (V2)',
       features: ['Obstacle avoidance', 'Waypoint system', 'Performance optimization'],
-      released: '2024-08-15',
-      playUrl: './v2/index.html'
+      released: '2026-07-1',
+      playUrl: './v2.0.0/index.html'
     },
     v3: {
       title: 'VERSION 3 (V3)',
